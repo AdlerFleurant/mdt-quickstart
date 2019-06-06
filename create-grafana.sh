6487c47d61dd655907d7a662d8b951b1615efb57
Adler Fleurant <afleuran@redhat.com>
2019-06-06 14:14:19 -0400

    Correcting bug in deployment script not pointing to correct files.

--- create-grafana.sh
+++ create-grafana.sh
@@ -2,7 +2,7 @@
 oc apply -f .openshift/manifests/grafana-sa.yml -n openshift-monitoring
 oc apply -f .openshift/manifests/grafana-clusterrolebinding.yml -n openshift-monitoring
 oc apply -f .openshift/manifests/grafana-custom-config-secret.yml -n openshift-monitoring
-oc apply -f .openshift/dashboard-devops.yml -n openshift-monitoring
+oc apply -f .openshift/manifests/dashboard-devops.yml -n openshift-monitoring
 oc apply -f .openshift/manifests/grafana-service.yml -n openshift-monitoring
 oc apply -f .openshift/manifests/grafana-route.yml -n openshift-monitoring
-oc apply -f .openshift/grafana-deployment.yml -n openshift-monitoring
+oc apply -f .openshift/manifests/grafana-deployment.yml -n openshift-monitoring
--- redeploy-grafana.sh
+++ redeploy-grafana.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 oc delete deployment.extensions/grafana-custom
-oc apply -f .openshift/dashboard-devops.yml
-oc apply -f .openshift/grafana-deployment.yml -n openshift-monitoring
+oc apply -f .openshift/manifests/dashboard-devops.yml -n openshift-monitoring
+oc apply -f .openshift/manifests/grafana-deployment.yml -n openshift-monitoring
